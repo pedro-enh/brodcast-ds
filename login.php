52e54cdf4dcdee09afaddb7eda21c61abf60a59b
pedro-enh <106911760+pedro-enh@users.noreply.github.com>
2025-08-11 16:30:24 +0100

    Update login.php

--- login.php
+++ login.php
@@ -7,14 +7,14 @@ try {
     $config = [
         'DISCORD_CLIENT_ID' => '1404415002269712394',
         'DISCORD_CLIENT_SECRET' => '_IkY6jLnzkm9Sm06km0-4pKsUcpfe9P8',
-        'REDIRECT_URI' => 'https://brodcast-discord-pro.42web.io/auth.php'
+        'REDIRECT_URI' => 'https://brodcast-ds-production.up.railway.app/auth.php'
     ];
 }
 
 // Discord OAuth URL - using the new auth-simple.php handler
 $discord_oauth_url = 'https://discord.com/api/oauth2/authorize?' . http_build_query([
     'client_id' => $config['DISCORD_CLIENT_ID'],
-    'redirect_uri' => 'https://brodcast-discord-pro.42web.io/auth-simple.php',
+    'redirect_uri' => 'https://brodcast-ds-production.up.railway.app/auth-simple.php',
     'response_type' => 'code',
     'scope' => 'identify'
 ]);
@@ -23,3 +23,4 @@ $discord_oauth_url = 'https://discord.com/api/oauth2/authorize?' . http_build_qu
 header('Location: ' . $discord_oauth_url);
 exit;
 ?>
+
